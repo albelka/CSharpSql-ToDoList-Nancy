fce947167d3a4b75be7468c46062cbd3b68ba918
Anne Belka and Steve Burton <anne+steve@>
2016-12-12 11:58:29 -0800

    writing delete .. . time for lunch

--- Tests/CategoryTest.cs
+++ Tests/CategoryTest.cs
@@ -66,8 +66,25 @@ namespace ToDoList
 
         List<Task> testTaskList = new List<Task> {firstTask, secondTask};
         List<Task> resultTaskList = testCategory.GetTasks();
-
       }
+      [Fact]
+      public void Test_Update_UpdatesCategoryInDatabase()
+      {
+        //Arrange
+        string name = "Home stuff";
+        Category testCategory = new Category(name);
+        testCategory.Save();
+        string newName = "Work stuff";
+
+        //Act
+        testCategory.Update(newName);
+
+        string result = testCategory.GetName();
+
+        //Assert
+        Assert.Equal(newName, result);
+      }
+
       [Fact]
       public void Test_Delete_DeletesCategoryFromDatabase()
       {
@@ -153,6 +170,35 @@ namespace ToDoList
 
       //Assert
       Assert.Equal(testTaskCategories, resultTaskCategories);
+    }
+    [Fact]
+    public void Test_Delete_DeletesCategoryFromDatabase()
+    {
+      //Arrange
+      string name1 = "Home stuff";
+      Category testCategory1 = new Category(name1);
+      testCategory1.Save();
+
+      string name2 = "Work stuff";
+      Category testCategory2 = new Category(name2);
+      testCategory2.Save();
+
+      Task testTask1 = new Task("Mow the lawn", DateTime.Today, testCategory1.GetId());
+      testTask1.Save();
+      Task testTask2 = new Task("Send emails", DateTime.Today, testCategory2.GetId());
+      testTask2.Save();
+
+      //Act
+      testCategory1.Delete();
+      List<Category> resultCategories = Category.GetAll();
+      List<Category> testCategoryList = new List<Category> {testCategory2};
+
+      List<Task> resultTasks = Task.GetAll();
+      List<Task> testTaskList = new List<Task> {testTask2};
+
+      //Assert
+      Assert.Equal(testCategoryList, resultCategories);
+      Assert.Equal(testTaskList, resultTasks);
     }
       public void Dispose()
       {
